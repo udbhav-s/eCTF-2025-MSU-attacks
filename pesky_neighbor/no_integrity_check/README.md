fcaae763941bd841dd1fcce94a276f886fc83d29
udbhav-s <udbhavs03@gmail.com>
2025-04-04 18:28:42 -0400

    Add a no integrity check attack for pesky neighbor

---
+++ pesky_neighbor/no_integrity_check/README.md
@@ -0,0 +1,13 @@
+# Overview
+
+Execution of a "Pesky Neighbor" attack against a team
+This attack exploits a design flaw where the integrity of frames isn't checked, and flipping a bit within the non-header encrypted data causes a valid decode.
+
+# Running
+
+Run the package script:
+```
+./package.sh team_name
+```
+
+Submit the resulting zip file to the pesky neighbor service. 
---
+++ pesky_neighbor/no_integrity_check/pesky_neighbor.py
@@ -0,0 +1,33 @@
+# Execution of a "Pesky Neighbor" attack against a team
+# This attack exploits a design flaw where instead of having a global timestamp counter for accepting frames
+# after a certain timestamp, there is instead one per channel. This allows us to send out of order frames
+# for one channel and then another, technically violating sequential timestamp requirements and earning a flag.
+
+import time
+import sys
+import json
+
+from ectf25.utils.decoder import DecoderIntf
+
+if __name__ == "__main__":
+    print("Running pesky neighbor attack")
+
+    time.sleep(1)
+
+    with open("frames.json") as f:
+        frames = json.loads(f.read())
+
+    ch1_frame = frames["ch1_frame"]
+
+    # In UCF, a frame is encoded_frame = channel (4) + iv (12) + body (64)
+    # Where body is max of size 80 (with 16 bytes padding)
+    frame_data = bytes.fromhex(ch1_frame["encoded"])
+    frame_data = frame_data[:50] + bytes([frame_data[50] ^ 0xff]) + frame_data[51:]
+
+    decoder = DecoderIntf(sys.argv[1])
+
+    decoder.list()
+
+    decoder.decode(frame_data)
+
+    print("Successfully decoded modified frame")
